25ee377b6af6ac8130b7427ba5f158080d90bf32
DumitrascuLuca <lucaddumitrascu@gmail.com>
2021-04-15 09:21:34 +0300

    seminar 8 final

--- Seminar8/src/ro/ase/cts/seminar8/Main.java
+++ Seminar8/src/ro/ase/cts/seminar8/Main.java
@@ -1,6 +1,13 @@
 package ro.ase.cts.seminar8;
 
+import ro.ase.cts.composite.CatalogComponent;
+import ro.ase.cts.composite.Product;
+import ro.ase.cts.composite.ProductCatalog;
 import ro.ase.cts.facade.OrderFacade;
+import ro.ase.cts.seminar8.decorator.AbstractBackpack;
+import ro.ase.cts.seminar8.decorator.BackPack;
+import ro.ase.cts.seminar8.decorator.WithFood;
+import ro.ase.cts.seminar8.decorator.WithLaptopDecorator;
 
 public class Main {
 
@@ -10,7 +17,34 @@ public class Main {
 		OrderFacade facade=new OrderFacade();
 		facade.order();
 		
+		CatalogComponent laptop=new Product("Asus", 1500);
+		CatalogComponent smartphone=new Product("Haifon", 10);
 		
+		CatalogComponent techProductsCatalog=new ProductCatalog("TechProducts");
+		
+		CatalogComponent officeProductCatalog=new ProductCatalog("OfficeProducts");
+		CatalogComponent paperclip=new Product("Paperclip", 10);
+		CatalogComponent pen=new Product("Pen", 20);
+		
+		techProductsCatalog.add(smartphone);
+		techProductsCatalog.add(laptop);
+		
+		officeProductCatalog.add(paperclip);
+		officeProductCatalog.add(pen);
+		
+		CatalogComponent genericProduct=new ProductCatalog("Generic");
+		genericProduct.add(new Product("generic product", 1));
+		genericProduct.add(techProductsCatalog);
+		genericProduct.add(officeProductCatalog);
+		System.out.println("\n-----------------------");
+		System.out.println(genericProduct.toString());
+		System.out.println("\n-----------------------");
+		
+		
+		AbstractBackpack myBackpack=new BackPack();
+		myBackpack.pack();
+		AbstractBackpack decoratedBackpack=new WithLaptopDecorator(new WithFood(myBackpack,"banana"));
+		decoratedBackpack.pack();
 	}
 
 }
---
+++ Seminar8/src/ro/ase/cts/seminar8/decorator/WithLaptopDecorator.java
@@ -0,0 +1,16 @@
+package ro.ase.cts.seminar8.decorator;
+
+public class WithLaptopDecorator extends AbstractBackPackDecorator{
+
+	public WithLaptopDecorator(AbstractBackpack basicBackPack) {
+		super(basicBackPack);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void pack() {
+		this.basic.pack();
+		
+	}
+
+}

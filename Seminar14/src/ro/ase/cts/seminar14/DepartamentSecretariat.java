40db910577e3a8f1ed938c04c11ec26ec98281df
DumitrascuLuca <lucaddumitrascu@gmail.com>
2021-06-03 08:22:14 +0300

    seminar14 break

---
+++ Seminar14/src/ro/ase/cts/seminar14/DepartamentSecretariat.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.seminar14;
+
+import java.util.ArrayList;
+
+public class DepartamentSecretariat {
+
+	ArrayList<Student> listastudenti;
+
+	public DepartamentSecretariat(ArrayList<Student> listastudenti) {
+		super();
+		this.listastudenti = listastudenti;
+	}
+	
+	public DepartamentSecretariat() {
+		
+		this.listastudenti = new ArrayList<Student>();
+	}
+	
+	
+	float calculBursa(int index) {
+		
+		return 0;
+	}
+}
